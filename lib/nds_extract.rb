f9fb73a4a0d553acdce64c310d4e52dd86534dd7
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-02-16 03:50:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -60,7 +60,7 @@ def movies_with_directors_set(source)
       movie_hash = {}
       movie_hash[:movie] = source[i][:movies][current_movie][:title]
       movie_hash[:director_name] = source[i][:name]
-      while 
+     
       
       movie_list.push(movie_hash) # [:movie => title, director_name => name]
       all_movies.push(movie_list)
